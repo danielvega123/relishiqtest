5bb0d51d339dcd262cdc28c0a5dd50baaf67258a
Daniel vega <dvega@GS1.LOCAL>
2022-12-23 22:07:53 -0600

    deploy test

--- backend/server/server.js
+++ backend/server/server.js
@@ -9,7 +9,7 @@
  */
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-const path = require('path')
+const serverless = require('serverless-http');
 
 dotenv.config()
 var express = require('express')
@@ -23,7 +23,11 @@ app.get('/', function (req, res) {
 // URL API
 app.use(require("../routes/index"));
 
+if(process.env.env === "dev"){
+    app.listen(process.env.PORT, () => {
+        console.log(`Escuchando el puerto ${process.env.PORT}`)
+    })
+}
 
-app.listen(process.env.PORT, () => {
-    console.log(`Escuchando el puerto ${process.env.PORT}`)
-})
+module.exports = app;
+module.exports.handler = serverless(app);
