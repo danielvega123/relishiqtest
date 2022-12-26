fa4f0345cbc775217ad94edd08391ec861b8d358
Daniel vega <dvega@GS1.LOCAL>
2022-12-26 16:18:34 -0600

    fix dependencies

--- backend/server/server.js
+++ backend/server/server.js
@@ -16,9 +16,11 @@ var express = require("express");
 var app = express();
 app.use(morgan("tiny"));
 
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 app.get("/", function (req, res) {
   res.send("hello, world!");
